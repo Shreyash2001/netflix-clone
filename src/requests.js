6508f7c0dc08a15d05535cefc3ae8ebda9024d02
Shreyash <shreyashkatare21@gmail.com>
2021-01-24 19:29:01 +0530

    first commit

---
+++ src/requests.js
@@ -0,0 +1,15 @@
+const API_KEY = "5b83527f20412342738089c4292ef2e5";
+
+
+const requests = {
+    fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=enUS`,
+    fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
+    fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=enUS`,
+    fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
+    fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
+    fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
+    fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
+    fetchDocumentaries: `/discover/movie?api_key=${API_KEY}&with_genres=99`, 
+}
+
+export default requests;
